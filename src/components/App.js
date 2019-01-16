02e6064f74c8c87a0fa7722fd3ffc76c0fefdca8
rulamon <ruben.lamon@gmail.com>
2019-01-16 11:07:34 +0100

    first commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+
+export class App extends React.Component {
+    constructor(props) {
+        super(props)
+    }
+    render() {
+        return(
+            <p>Hallo Wereldhjeemlqskdjfqmlsdkfjm!</p>
+        )
+    }
+}
